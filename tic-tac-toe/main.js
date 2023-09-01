558e563d0f3f147d2d1546bb2232aa34ab41fa94
myriam graine <myriam.bdn@gmail.com>
2023-09-01 18:20:05 +0200

    const winning paterns

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -6,6 +6,17 @@ const playerOne = 'X';
 const playerTwo = 'O';
 let playerTurn = playerOne;
 
+const winningPatterns = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
 cells.forEach(cell => {
   cell.addEventListener('click', playGame, { once: true});
 });
@@ -13,8 +24,23 @@ cells.forEach(cell => {
 function playGame(e) {
   e.target.innerHTML = playerTurn;
 
+  if (checkWin(playerTurn)) {
+    updateGameStatus("wins + playerTurn");
+    return endGame();
+  } else if (checkDraw()) {
+    updateGameStatus("draw");
+    return endGame();
+  }
+
   updateGameStatus(playerTurn);
   playerTurn == playerOne ? playerTurn = playerTwo : playerTurn = playerOne;
+}
+
+function checkWin(playerTurn) {}
+function checkDraw() {
+  return [...cells].every(celle => {
+    return cell.innerHTML == playerOne || cell.innerHTML == playerTwo;
+  });
 }
 
 function updateGameStatus(status) {
@@ -41,3 +67,6 @@ function updateGameStatus(status) {
   gameStatus.innerHTML = statusText;
   endgameStatus.innerHTML = statusText;
 }
+
+function endGame() { document.getElementById('gameEnd').style.display = "block"}
+function reloadGame() { window.location.reload()}
